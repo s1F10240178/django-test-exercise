6a991dbbeb2334fbe804e491ebe33e7bf70cb692
1f10240178 <s1f102401786@iniad.org>
2025-07-02 16:24:51 +0900

    Taskモデルを追加

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    posted_at = models.DateTimeField(default=timezone.now)
+    completed = models.BooleanField(default=False)
+    due_at= models.DateTimeField(null=True, blank=True)
+    
--- todo/tests.py
+++ todo/tests.py
@@ -1,6 +1,26 @@
 from django.test import TestCase
+from django.utils import timezone
+from todo.models import Task
+from datetime import datetime
+
 
 # Create your tests here.
-class SampleTestCase(TestCase):
-    def test_sample(self):
-        self.assertEqual(1 + 1, 2)
+
+class TaskModelTestCase(TestCase):
+    def test_task_creation(self):
+      due= timezone.make_aware(datetime(2024,6,30,23,59,59))
+      task=Task(title='task1',due_at=due)
+      task.save()
+      task=Task.objects.get(pk=task.pk)
+      self.assertEqual(task.title, 'task1')
+      self.assertEqual(task.due_at, due)
+      self.assertFalse(task.completed)
+   
+    def test_create_task2(self):
+        task = Task(title='task2')
+        task.save()
+        task = Task.objects.get(pk=task.pk)
+        self.assertEqual(task.title, 'task2')
+        self.assertIsNone(task.due_at,None)
+        self.assertFalse(task.completed)
+ 
