4f4902bc17071b0374f809fe956a4612079a89b3
1f10240178 <s1f102401786@iniad.org>
2025-07-09 16:56:37 +0900

    詳細ページを追加

--- todo/tests.py
+++ todo/tests.py
@@ -97,6 +97,7 @@ class TaskViewTestCase(TestCase):
         task.save()
         client = Client()
         response = client.get('/{}/'.format(task.pk))
+
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/detail.html')
         self.assertEqual(response.context['task'], task)
@@ -104,4 +105,5 @@ class TaskViewTestCase(TestCase):
     def test_detail_get_fail(self):
         client = Client()
         response = client.get('/1/')
+        
         self.assertEqual(response.status_code, 404)
